0bf64f4064c111ce855931165920301938649090
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-18 08:33:03 +0200

    created appp and adding app componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import Footer from "./Footer"
+import Hero from "./Hero"
 import Nav from "./Nav"
+import Products from "./Products"
 
 function App() {
+ 
   return (
     <>
     <Nav/>
+    <main>
+    <Hero/>
+    <Products/>
+      <Footer/>
+    </main>
     </>
   )
 }
