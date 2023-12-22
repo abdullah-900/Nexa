1ad38a3b3c13a5a05845bfa9c07e955d201b04bf
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-22 19:47:34 +0200

    add multiple products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,8 +4,8 @@ const Products = () => {
   return (
     <section className=' p-14 flex flex-col justify-center items-center gap-10'>
       <h2 className='text-[2.6875rem] leading-[2.6875rem] font-Marcellus'>Our Products</h2>
-      <div className=' md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  md:gap-10' >
-        <Product img={'lip1.jpeg'} name={'Rose Shimmer'} price={'200 EGP'}/>
+      <div className='grid gap-10 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-12' >
+        <Product img={'lip1.jpeg'} name={'Rose Shimmer'} price={'200 EGP'} />
         <Product img={'lip2.jpeg'} name={'Coral Crush'} price={'230 EGP'}/>
         <Product img={'lip3.jpeg'} name={'Nude Elegance'} price={'150 EGP'}/>
         <Product img={'lip4.jpeg'} name={'Pink Petal'} price={'140 EGP'}/>
