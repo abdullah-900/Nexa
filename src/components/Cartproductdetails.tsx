1490da3fa763471957e0dface38f962e5fa6b891
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-22 19:35:55 +0200

    created carrtproducts and cartproducts details

---
+++ src/components/Cartproductdetails.tsx
@@ -0,0 +1,18 @@
+import Controls from "./Controls"
+
+const Cartproductdetails = () => {
+  return (
+    <article className="flex  justify-evenly items-center">
+    <header className="flex justify-center  gap-5 ">
+    <img src="lip1.jpeg" className="w-[6.25rem] h-[6.25rem] md:w-[12.5rem] md:h-[12.5rem] rounded-xl" alt="" />
+    <div className="flex flex-col   gap-5 w-full">
+    <h2 className="font-bold font-Causten text-[1.125rem]">Rose Shimmer</h2>
+    <Controls/>
+    </div>
+    </header>
+    <strong className="text-3c text-[1.125rem] leading-6 font-bold font-Causten  " >200 EGP</strong>
+       </article>
+  )
+}
+
+export default Cartproductdetails
---
+++ src/components/Cartproducts.tsx
@@ -0,0 +1,16 @@
+import Cartproductdetails from "./Cartproductdetails";
+import Subtotal from "./Subtotal";
+
+const Cartproducts = () => {
+  return (
+    <section className=" flex flex-col w-full justify-center  gap-[4.375rem] mb-[4.375rem]   ">
+    <header className="text-f6 bg-3c flex items-center justify-center h-[4.75rem]">
+      <h1 className="text-[1.125rem] font-semibold font-Causten">Shopping Cart</h1>
+    </header>
+<Cartproductdetails/>
+<Subtotal/>
+    </section>
+  );
+};
+
+export default Cartproducts;
