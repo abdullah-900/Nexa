cc6908d24a7cd93834b3d04e38c5c418434e9eab
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-23 09:14:51 +0200

    fix margin for cart

--- src/components/Cartproducts.tsx
+++ src/components/Cartproducts.tsx
@@ -3,7 +3,7 @@ import Subtotal from "./Subtotal";
 
 const Cartproducts = () => {
   return (
-    <section className=" flex flex-col w-full justify-center  gap-[4.375rem] mb-[4.375rem]   ">
+    <section className=" flex flex-col w-full justify-center  gap-[4.375rem] mt-[3.125rem]">
     <header className="text-f6 bg-3c flex items-center justify-center h-[4.75rem]">
       <h1 className="text-[1.125rem] font-semibold font-Causten">Shopping Cart</h1>
     </header>
