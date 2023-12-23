d9702a9333bba5defef23a7acae88376c090ce4d
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-23 09:15:38 +0200

    adding checkoutpage and its route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Homepage from "./pages/Homepage";
 import Cartpage from "./pages/Cartpage";
-
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Checkoutpage from "./pages/Checkoutpage";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Cartpage" element={<Cartpage />} />
+         <Route path="/Checkoutpage" element={<Checkoutpage />} /> 
       </Routes>
     </BrowserRouter>
   );
