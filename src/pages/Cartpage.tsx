497d8c814841ead9e576c9224778cc8d458df921
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-23 09:17:26 +0200

    added cartnav component to cartpage

--- src/pages/Cartpage.tsx
+++ src/pages/Cartpage.tsx
@@ -2,11 +2,13 @@
 import Nav from "../components/Nav"
 import Footer from "../components/Footer"
 import Cartproducts from "../components/Cartproducts"
+import Cartnav from "../components/Cartnav"
 const Cartpage = () => {
   return (
    <>
    <Nav cartbgcolor="bg-8a" carticoncolor="#FFFFFF"/>
    <main>
+    <Cartnav/>
     <Cartproducts/>
    </main>
    <Footer/>
