759f08ece57f6c62eb762f107cb5b464a9d56e12
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-23 09:16:44 +0200

    created checkout page

---
+++ src/pages/Checkoutpage.tsx
@@ -0,0 +1,15 @@
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+const Checkoutpage = () => {
+  return (
+    <>
+    <Nav cartbgcolor="bg-8a" carticoncolor="#FFFFFF"/>
+    <main>
+    
+    </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default Checkoutpage
