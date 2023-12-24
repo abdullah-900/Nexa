e2ec26f8e623aa7f8fc73047c3b1f204ab82941b
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-24 04:03:59 +0200

    fix overflow issue in mobile

--- src/components/Cartnav.tsx
+++ src/components/Cartnav.tsx
@@ -2,15 +2,15 @@
 import { Link } from "react-router-dom"
 const Cartnav = () => {
   return (
-   <article className="flex flex-col gap-[30px] w-full md:w-[30.125rem]   h-[5.6875rem] ml-[1.25rem] md:ml-[6.25rem] mt-[3.125rem]  ">
+   <article className="flex flex-col gap-[30px]  md:w-[30.125rem]   h-[5.6875rem] pl-4 md:ml-[6.25rem] mt-[3.125rem]  ">
 <div className="flex gap-[.9375rem]">
-  <Link to="/" className="font-Causten text-80 text-[1.125rem] font-medium leading-[1.375rem] ">Home</Link>
+  <Link to="/" className="font-Causten text-80 text-[1.125rem] font-medium leading-normal ">Home</Link>
   <img src="left (Stroke).svg" alt="" />
-  <a className="text-3c font-Causten text-[1.125rem] leading-[1.375rem]">Add To Cart</a>
+  <a className="text-3c font-Causten text-[1.125rem] leading-normal">Add To Cart</a>
 </div>
 <div className="flex flex-col gap-[.3125rem]">
-  <p className="text-[.875rem] select-none font-Causten leading-[1.0625rem]   tracking-[.0175rem] text-80">Please fill in the fields below and click place order to complete your purchase</p>
-  <p className="text-[.875rem] font-Causten leading-[1.0625rem]  tracking-[.0175rem] text-80">Already registered? Please login here</p>
+  <p className="text-[.875rem] select-none font-Causten leading-normal   tracking-[.0175rem] text-80">Please fill in the fields below and click place order to complete your purchase</p>
+  <p className="text-[.875rem] font-Causten leading-normal  tracking-[.0175rem] text-80">Already registered? Please login here</p>
 </div>
    </article>
   )
