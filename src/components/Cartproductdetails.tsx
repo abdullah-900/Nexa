a5a2fc623147ccc02ccc6f358649e641131c2809
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2023-12-25 21:03:35 +0200

    change product name font color

--- src/components/Cartproductdetails.tsx
+++ src/components/Cartproductdetails.tsx
@@ -6,7 +6,7 @@ const Cartproductdetails = () => {
     <header className="flex justify-center  gap-5 ">
     <img src="lip1.jpeg" className="w-[6.25rem] h-[6.25rem] md:w-[12.5rem] md:h-[12.5rem] rounded-xl" alt="" />
     <div className="flex flex-col   gap-5 w-full">
-    <h2 className="font-bold font-Causten text-[1.125rem]">Rose Shimmer</h2>
+    <h2 className="font-bold font-Causten text-[1.125rem] text-3c">Rose Shimmer</h2>
     <Controls/>
     </div>
     </header>
