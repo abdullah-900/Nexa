a4967f5faa2c0a25962c6ad8bb4a9a8b05f62d76
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2024-01-29 13:13:16 +0200

    make cartpage auto nav to top

--- src/pages/Cartpage.tsx
+++ src/pages/Cartpage.tsx
@@ -1,9 +1,16 @@
-
+import { useLayoutEffect } from "react"
 import Nav from "../components/Nav"
 import Footer from "../components/Footer"
 import Cartproducts from "../components/Cartproducts"
 import Cartnav from "../components/Cartnav"
 const Cartpage = () => {
+  useLayoutEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // Optional: Add smooth scrolling
+    });
+  }, []); // Empty dependency array to run only once after render
+  
   return (
    <>
    <Nav cartbgcolor="bg-8a" carticoncolor="#FFFFFF"/>
