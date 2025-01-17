e346940fb2e6cd5e415785d871078e3376697571
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2025-01-17 07:44:55 +0200

    refactor: wrap App component with MyProvider for context management

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+ // @ts-nocheck
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import {MyProvider} from './Context/Context.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  
   <React.StrictMode>
+    <MyProvider>
     <App />
+    </MyProvider>
   </React.StrictMode>,
 )
