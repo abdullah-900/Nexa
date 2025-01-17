2c368d72ba148a770872fe5ca215d6317a6a8bd1
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2025-01-17 07:44:33 +0200

    refactor: update Footer component layout and add social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-   
+  
     <footer className="bg-46  gap-10 lg:gap-20 flex flex-col items-center justify-center   w-full font-Causten text-f6 px-[3.125rem]  lg:px-[6.875rem] py-[3.6875rem]"  >
-      <section className="  flex-col lg:flex-row items-center flex gap-10 lg:gap-20 ">
+      <section className=" grid md:grid-cols-2 lg:grid-cols-4   gap-10 lg:gap-20 ">
         <section className="flex flex-col items-center  lg:items-start text-center lg:text-left" >
           <h2 className="leading-[3.8742rem] font-bold text-[1.7862rem]">
             Need Help
@@ -25,7 +25,6 @@ const Footer = () => {
             <li>euphoriastan</li>
             <li>Collaboration</li>
             <li>Media</li>
-            <li>&nbsp;</li>
           </ul>
         </section>
         <section className="flex flex-col items-center lg:items-start text-center lg:text-left">
@@ -37,7 +36,6 @@ const Footer = () => {
             <li>Privacy Policy</li>
             <li>Shipping Policy</li>
             <li>Sitemap</li>
-            <li>&nbsp;</li>
           </ul>
         </section>
         <section className="flex flex-col items-center lg:items-start text-center lg:text-left">
@@ -48,25 +46,26 @@ const Footer = () => {
             <li>support@euphoria.in</li>
             <li>Eklingpura Chouraha, Ahmedabad Main Road</li>
             <li>(NH 8- Near Mahadev Hotel)</li>
-            <li>&nbsp;</li>
-            <li>&nbsp;</li>
           </ul>
         </section>
       </section>
       <section className="flex flex-col lg:flex-row gap-10 w-full justify-between items-center">
         <section className="flex justify-center items-center gap-[.625rem]">
-          <img src="facebook.svg" alt="facebook icon" />
-          <img src="insta.svg" alt="insta icon" />
-          <img src="x.svg" alt=" twitter icon" />
-          <div className="bg-f6 rounded-[.6675rem] w-[2.314rem] h-[2.314rem] flex justify-center items-center">
-            <span className="text-[#363E3E] font-Roboto leading-[1.78rem] text-[1.068rem] font-bold tracking-[.0214rem]">
-              in
-            </span>
-          </div>
+          <a href="https://facebook.com/"><img src="facebook.svg" alt="facebook icon" /></a>
+          <a href="https://instagram.com/"><img src="insta.svg" alt="insta icon" /></a>
+          <a href="https://x.com/"><img src="x.svg" alt=" twitter icon" /></a>
+          <a href="https://linkedin.com/">
+            <div className="bg-f6 rounded-[.6675rem] w-[2.314rem] h-[2.314rem] flex justify-center items-center">
+              <span className="text-[#363E3E] font-Roboto leading-[1.78rem] text-[1.068rem] font-bold tracking-[.0214rem]">
+                in
+              </span>
+            </div>
+          </a>
         </section>
         <section className="flex flex-col gap-4">
           <h2 className="font-bold text-[1.7862rem] leading-[3.8742rem]">Download The App </h2>
          <div className="flex gap-[1.3274rem]">
+         <a href="#">
          <div className="flex justify-around  items-center  w-[9.6101rem] h-[3.2024rem] bg-[#404040] rounded-xl">
           <img className="w-[1.8533rem] h-[2.061rem]" src="playicon.svg" alt="playicon" />
            <div className="flex justify-start flex-col">
@@ -74,6 +73,8 @@ const Footer = () => {
            <span className="text-[1.125rem]">Google Play</span>
            </div>
           </div>
+          </a>
+          <a href="#">
           <div className="flex justify-around  items-center  w-[9.6101rem] h-[3.2024rem] bg-[#404040] rounded-xl">
           <img className="w-[1.8533rem] h-[2.061rem]" src="iphone.svg" alt="playicon" />
            <div className="flex justify-start flex-col">
@@ -81,6 +82,7 @@ const Footer = () => {
            <span className="text-[1.125rem]">App Store</span>
            </div>
           </div>
+          </a>
          </div>
         </section>
       </section>
